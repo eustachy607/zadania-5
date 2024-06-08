2aee98fb7019c4ab524e2c85500f6694c54f64ac
eustachy607 <164636124+eustachy607@users.noreply.github.com>
2024-06-08 22:14:18 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const fs = require('fs');
-const path = require('path');
 
-const app = new Koa();
-const router = new Router();
+const chalk = require('chalk');
 
-app.use(bodyParser());
-
-router.get('/login', async (ctx) => {
-    ctx.type = 'html';
-    ctx.body = fs.createReadStream(path.join(__dirname, 'login.html'));
-});
-
-router.post('/login', async (ctx) => {
-    const { username, password } = ctx.request.body;
-
-    if (password.length < 8) {
-        ctx.body = 'To hasło jest zbyt krótkie';
-        return;
-    }
-
-    if (username === 'admin' && password === 'adminadmin') {
-        ctx.body = 'Zalogowany!';
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(chalk.magenta(i));
+    } else if (i % 3 === 0) {
+        console.log(chalk.red(i));
+    } else if (i % 5 === 0) {
+        console.log(chalk.blue(i));
     } else {
-        ctx.body = 'Nieprawidłowa nazwa użytkownika lub hasło';
+        console.log(chalk.white(i));
     }
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+}
