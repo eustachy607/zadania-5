6c5284c2dc1831f352e98359a05b4c4ce7f93b97
eustachy607 <164636124+eustachy607@users.noreply.github.com>
2024-06-08 22:16:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const fs = require('fs');
+const path = require('path');
 
-const chalk = require('chalk');
+const app = new Koa();
+const router = new Router();
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(chalk.magenta(i));
-    } else if (i % 3 === 0) {
-        console.log(chalk.red(i));
-    } else if (i % 5 === 0) {
-        console.log(chalk.blue(i));
-    } else {
-        console.log(chalk.white(i));
+app.use(bodyParser());
+
+router.get('/login', async (ctx) => {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream(path.join(__dirname, 'login.html'));
+});
+
+router.post('/login', async (ctx) => {
+    const { username, password } = ctx.request.body;
+
+    if (password.length < 8) {
+        ctx.body = 'To has³o jest zbyt krótkie';
+        return;
     }
-}
+
+    if (username === 'admin' && password === 'adminadmin') {
+        ctx.body = 'Zalogowany!';
+    } else {
+        ctx.body = 'Nieprawid³owa nazwa u¿ytkownika lub has³o';
+    }
+});
+
+router.put('/login', async (ctx) => {
+    ctx.body = 'Odebra³em polecenie PUT';
+});
+
+router.delete('/login', async (ctx) => {
+    ctx.body = 'Odebra³em polecenie DELETE';
+});
+
+router.patch('/login', async (ctx) => {
+    ctx.body = 'Odebra³em polecenie PATCH';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
